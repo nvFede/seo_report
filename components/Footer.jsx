dc02f0db05fa2f366de798c34b0b4b8095de3ae3
Federico <nvfede@gmail.com>
2023-01-21 17:31:16 +0100

    fix bug

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,17 +5,28 @@ const Footer = () => {
     <footer className="w-full bg-slate-50 text-center mt-5">
       <p className="p-2">
         Made by
-        <a target={'_blank'} className="text-teal-600" href="https://github.com/nvFede">
+        <a
+          target={"_blank"}
+          rel="noreferrer"
+          className="text-teal-600"
+          href="https://github.com/nvFede"
+        >
           {" "}
           Fede.rico
         </a>
       </p>
       <p className="p-2">
         Special thanks to
-        <a target={'_blank'} className="text-teal-600" href="https://twitter.com/matspeterforss">
+        <a
+          target={"_blank"}
+          rel="noreferrer"
+          className="text-teal-600"
+          href="https://twitter.com/matspeterforss"
+        >
           {" "}
-          Mats-Peter Forss 
-        </a>{" "} for the curated list
+          Mats-Peter Forss
+        </a>{" "}
+        for the curated list
       </p>
     </footer>
   );
